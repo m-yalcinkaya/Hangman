c6358dd8dfef842c8802d5a2d6d419d00f0c9d46
Mustafa Yalçınkaya <myalcinkaya136@gmail.com>
2024-03-11 13:14:56 +0300

    completed basic operations

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ localStorage.clear();
 localStorage.setItem('key', 'istanbul');
 localStorage.setItem('wrongLetters', '');
 const key = localStorage.getItem('key');
+let shapeCount = document.getElementsByClassName('visible-item');
+let spanCount = document.getElementsByClassName('situated');
+console.log(spanCount);
 
-
-
+            
 for(let i=0; i<key.length; i++){
     let element = document.createElement('div');
     element.setAttribute('class', 'letter');
@@ -18,13 +20,13 @@ for(let i=0; i<key.length; i++){
     word.appendChild(element);
 }
 
-    const screen = document.querySelector('body');
-    screen.addEventListener('keydown',getLetter);
+const screen = document.querySelector('body');
+screen.addEventListener('keydown',getLetter);
 
 
-    var isAlpha = function(ch){
-        return typeof ch === "string" && ch.length === 1 && /[A-Za-z]/.test(ch);
-      }
+var isAlpha = function(ch){
+    return typeof ch === "string" && ch.length === 1 && /[A-Za-z]/.test(ch);
+}
 
 
 
@@ -33,14 +35,53 @@ function getLetter(e){
     if(isAlpha(letter)){
         if(!key.includes(letter)){
             addWrongLetter(letter);
+            drawShape();
         }else{
             for(let i=0; i<key.length; i++){
                 if(key[i]===letter){
                     word.children[i].children[0].setAttribute('class', '')
                 }
             }
+            if(spanCount.length == 0){
+                myPopup(true).show();
+            }
         }
     }
+}
+
+
+
+function drawShape(){
+    let shape = document.querySelector('.visible-item');
+            shape.setAttribute('class', 'item');
+            if(shapeCount.length === 0){
+                setTimeout(function (){
+                    myPopup(false).show();
+                } ,10);
+            }
+}
+
+
+function myPopup(isWon){
+
+    return new Popup({
+        title:  isWon ? 'Congratulations' : 'Game Over',
+        backgroundColor: isWon ? '#32cd32' : '#ff0000',
+        titleColor: '#fff',
+        textColor: '#fff',
+        content: isWon ? `You know the word
+        {btn-popup-button}[New Game]` : `
+        You didn't know the word
+            {btn-popup-button}[New Game]`,
+    
+        loadCallback: () => {
+            const button = document.querySelector(".popup-button");
+            button.addEventListener("click", () => {
+                window.location.reload();
+            });
+        },
+    });
+
 }
 
 
